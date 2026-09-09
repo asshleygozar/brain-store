b18a87df473b96adeae935ab00fd5e20cf750c80
Asshley <asshleydeanwaynegozar@gmail.com>
2026-09-09 21:40:18 +0800

    feat: implement api authentication and issuance of secrets

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
-from .routers import ingest_router
+from fastapi import APIRouter, Depends
+from .routers import ingest_router, admin_router
 
 api_router = APIRouter()
 
 api_router.include_router(ingest_router, prefix='/ingest', tags=['Ingest'])
+api_router.include_router(admin_router, prefix='/admin', tags=['Admin'])
